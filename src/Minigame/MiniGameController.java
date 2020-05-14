440f6453c7e5d50e7ab1f339b45800a4f6320667
sanghyun.Park <take6948@google.com>
2020-05-14 16:38:57 +0900

    미니게임 수정 - ladderResult.fxml 추가 - minigameController 오류 수정

--- src/Minigame/MiniGameController.java
+++ src/Minigame/MiniGameController.java
@@ -6,8 +6,12 @@ import java.util.ResourceBundle;
 
 import MainMenu.MainPage.MainPageController;
 import Minigame.Roulette.RouletteMenuController;
+import Minigame.Service.BrkacornServiceImpl;
+import Minigame.Service.LadderServiceImpl;
 import Minigame.Service.MinigameCommonServiceImpl;
 import Minigame.Service.MinigameService;
+import Minigame.ServiceImpl.BrkacornService;
+import Minigame.ServiceImpl.LadderService;
 import Minigame.ServiceImpl.MinigameCommonService;
 import Minigame.ServiceImpl.MinigameServiceImpl;
 import javafx.event.ActionEvent;
@@ -23,6 +27,8 @@ public class MiniGameController implements Initializable{
 	private Parent root;
 	private MainPageController mainCon;
 	private MinigameCommonService commonService;
+	private LadderService ladderServ;
+	private BrkacornService brkAcornServ;
 	
 	public void setRoot(Parent root){
 		this.root = root;
@@ -35,6 +41,8 @@ public class MiniGameController implements Initializable{
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		commonService = new MinigameCommonServiceImpl();	
+		ladderServ = new LadderServiceImpl();
+		brkAcornServ = new BrkacornServiceImpl();
 	}
 	
 	public void RouletteBtnPressed(ActionEvent event) throws IOException {
@@ -54,5 +62,14 @@ public class MiniGameController implements Initializable{
 		
 		menuStage.setScene(new Scene(menuRoot));
 		menuStage.show();
+	}
+	public void LadderBtnPressed() {
+		commonService.closeWindow(root);
+		ladderServ.runLadder();
+	}
+	
+	public void BrkAcronBtnPressed() {
+		commonService.closeWindow(root);
+		brkAcornServ.runBrkacorn();
 	}
 }
