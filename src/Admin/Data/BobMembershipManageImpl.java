fd52057c4bb6f0491f57b5cda6c6f34231c1971a
sanghyun.Park <take6948@google.com>
2020-05-18 14:24:12 +0900

    admin page -  Data 수정

--- src/Admin/Data/BobMembershipManageImpl.java
+++ src/Admin/Data/BobMembershipManageImpl.java
@@ -5,13 +5,14 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import Admin.AdminLogin.AddAddress;
 import Admin.AdminLogin.Member;
 
 
 
 public class BobMembershipManageImpl implements IBobMembershipManage {
 	final static String DRIVER = "org.sqlite.JDBC";
-	final static String DB = "jdbc:sqlite:C:/AcornDB/ProjectDB.db";
+	final static String DB = "jdbc:sqlite:src/Restaurant.db";
 	Connection conn;
 	public BobMembershipManageImpl() {
 		try {
@@ -27,22 +28,20 @@ public class BobMembershipManageImpl implements IBobMembershipManage {
 	}
 	@Override
 	public boolean BobMembershipProc(Member member) {
-		String sql = 	"INSERT INTO Bob "+
-						"(BobNum, BobImage, BobName, BobPhone, BobAddress, BobLocate, BobMain, Category, Date) "+
-						"VALUES (?,?,?,?,?,?,?,?,?)";
+		String sql = 	"INSERT INTO restaurant "+
+				"(name, name_id, category_id, price, businesshour, breaktime, image) "+
+				"VALUES (?,?,?,?,?,?,?)";
 		
 		try {
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 			
-			pStmt.setString(1, member.getBobNum());
-			pStmt.setString(2, member.getBobImage());
-			pStmt.setString(3, member.getBobName());
-			pStmt.setString(4, member.getBobPhone());
-			pStmt.setString(5, member.getBobAddress());
-			pStmt.setString(6, member.getBobLocate());
-			pStmt.setString(7, member.getBobMain());
-			pStmt.setString(8, member.getCategory());
-			pStmt.setString(9, member.getDate());
+			pStmt.setString(1, member.getName());
+			pStmt.setString(2, member.getName_id());
+			pStmt.setString(3, member.getCategory_id());
+			pStmt.setString(4, member.getPrice());
+			pStmt.setString(5, member.getBusinesshour());
+			pStmt.setString(6, member.getBreaktime());
+			pStmt.setString(7, member.getImage());
 			
 			
 			pStmt.executeUpdate();
@@ -65,5 +64,29 @@ public class BobMembershipManageImpl implements IBobMembershipManage {
 		
 	}
 	
+	@Override
+	public boolean BobAddAddressProc(AddAddress member1) 
+		{String sql = 	"INSERT INTO location "+
+						"(name_id, loca_x, loca_y, address) "+
+						"VALUES (?,?,?,?)";
 
+	try {
+		PreparedStatement pStmt1 = conn.prepareStatement(sql);
+
+		pStmt1.setString(1, member1.getName_id());
+		pStmt1.setString(2, member1.getLoca_x());
+		pStmt1.setString(3, member1.getLoca_y());
+		pStmt1.setString(4, member1.getAddress());
+
+		pStmt1.executeUpdate();
+
+		pStmt1.close();
+		conn.close();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		//e.printStackTrace();
+		return false;
+	}
+	return true;
+	}
 }
